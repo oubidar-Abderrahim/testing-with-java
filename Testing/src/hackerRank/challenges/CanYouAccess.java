fb4a073593419ffac20b3bda98d799f856f975ae
aoubidar <aoubidar@MAPF1GTABW.CSCMWS.CSCMWS.COM>
2019-03-18 10:14:18 +0100

    can you access (reflection)

---
+++ Testing/src/hackerRank/challenges/CanYouAccess.java
@@ -0,0 +1,71 @@
+package hackerRank.challenges;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
+import java.security.Permission;
+
+public class CanYouAccess {
+
+	public static void main(String[] args) throws Exception {
+		DoNotTerminate.forbidExit();
+
+		try {
+			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+			int num = Integer.parseInt(br.readLine().trim());
+			Object o;// Must be used to hold the reference of the instance of the class
+						// Solution.Inner.Private
+
+			CanYouAccess.Inner inner = new CanYouAccess.Inner();
+			
+			Class<?> privateInner = CanYouAccess.Inner.class.getDeclaredClasses()[0];
+			Constructor<?> constructor = privateInner.getDeclaredConstructors()[0];
+			constructor.setAccessible(true);
+			
+			o = constructor.newInstance(inner);
+			
+			Method powerof2 = o.getClass().getDeclaredMethods()[0];
+			
+			powerof2.setAccessible(true);
+			
+			System.out.println(num + " is " + powerof2.invoke(o, num));
+			
+ 			System.out.println("An instance of class: " + o.getClass().getCanonicalName() + " has been created");
+
+		} // end of try
+
+		catch (DoNotTerminate.ExitTrappedException e) {
+			System.out.println("Unsuccessful Termination!!");
+		}
+	}// end of main
+
+	static class Inner {
+		private class Private {
+			private String powerof2(int num) {
+				return ((num & num - 1) == 0) ? "power of 2" : "not a power of 2";
+			}
+		}
+	}// end of Inner
+
+}// end of Solution
+
+class DoNotTerminate { // This class prevents exit(0)
+
+	public static class ExitTrappedException extends SecurityException {
+
+		private static final long serialVersionUID = 1L;
+	}
+
+	public static void forbidExit() {
+		final SecurityManager securityManager = new SecurityManager() {
+			@Override
+			public void checkPermission(Permission permission) {
+				if (permission.getName().contains("exitVM")) {
+					throw new ExitTrappedException();
+				}
+			}
+		};
+		System.setSecurityManager(securityManager);
+	}
+}
